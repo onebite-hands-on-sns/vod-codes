a725e0c0500496ec4ba139e6408ea76bd2974b08
winterlood <king199777@gmail.com>
2025-06-26 15:31:01 +0900

    ~3.2

---
+++ section02/chapter04/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+     <Button>버튼!</Button>
+    </div>
+  );
+}
+
+export default App;
